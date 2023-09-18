09db8e5bbf51c873c5b29e047ba3bc88d35d8ea1
Taohookaitun <100595178+Taohookaitun@users.noreply.github.com>
2023-09-19 04:24:18 +0700

    Delete qr.js

--- qr.js
+++
@@ -1,8 +0,0 @@
-function generateQRCode() {
-    var qrText = document.getElementById("qr-text").value;
-    var qrcode = new QRCode(document.getElementById("qrcode"), {
-        text: qrText,
-        width: 128,
-        height: 128
-    });
-}
